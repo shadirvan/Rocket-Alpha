fe9040543b5aa7ffb423d409daba2234cd252911
Shadirvan <shadirvan2021@gmail.com>
2022-08-20 09:02:17 -0700

    completed the course of this project
    fixed nan error when period is set to zero

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -20,6 +20,7 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period == 0){return;}
         float cycles = Time.time / period; // continually growing over time.
 
         const float tau = Mathf.PI *2; // constan value of 6.28
